e37e3619ebee9e5b5c7f0f2137fe6bc6a370cefc
Vadym Lavorchuk <lavorchukvadim@gmail.com>
2021-01-19 21:19:53 +0200

    very basic layout

---
+++ src/components/step1/step1.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './step1.styles.scss';
+
+
+const Step1 = () => {
+    return (
+        <div>TEST</div>
+    );
+}
+
+export default Step1;
---
+++ src/pages/main/main.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './main.styles.scss';
+
+import Step1 from '../../components/step1/step1.component';
+
+const MainPage = () => {
+    return (
+        <div className="main">
+            <Step1/>
+        </div>
+    );
+}
+
+export default MainPage;
