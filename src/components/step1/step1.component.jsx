8009a93a736fa22507dc66eddeeffc7bd3ad8941
Vadym Lavorchuk <lavorchukvadim@gmail.com>
2021-01-26 23:20:05 +0200

    added image upload from function to class

--- src/components/step1/step1.component.jsx
+++ src/components/step1/step1.component.jsx
@@ -1,171 +1,238 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import ImageUploader from 'react-images-upload';
+
 import './step1.styles.scss';
 import image1 from './image1.jpg';
 import image2 from './image2.jpg';
 import image3 from './image3.jpg';
+import type1 from './type1.png';
+import type2 from './type2.png';
 
-const Step1 = () => {
-    const allSteps = 5;
+class Step1 extends React.Component {
 
-    const [step, setStep] = useState(0);
-    const [price, setPrice] = useState(300);
-
-    const [type, setType] = useState('type 1');
-    const [background, setBackground] = useState('apple');
-
-    const handleStepChange = (newStep) => {
-        setStep(newStep);
+    constructor(props) {
+        super(props);
+        this.state = {
+            step: 0,
+            price: 300,
+            type: '',
+            picture: '',
+            allSteps: 5,
+        }
     }
 
-    const handleTypeChange = (event) => {
-        setType(event.currentTarget.value);
+    handleStepChange = (newStep) => {
+        this.setState({
+            step: newStep,
+        })
     }
 
-    const handleBackgroundChange = (event) => {
-        setType(event.currentTarget.value);
+    handleTypeChange = (event) => {
+        this.setState({
+            type: event.currentTarget.value
+        })
     }
 
-    return (
-        <React.Fragment>
-            <header className="header">
-                {/* will insert logo and something more */}
-            </header>
+    handleImageUpload = (pictureFiles, pictureDataURLs) => {
+        this.setState({
+            picture: pictureDataURLs[0]
+        }, () => {
+            console.log(this.picture);
+        });
+    }
 
-            {step === 0 ? (<div>
-                <article className="content">
-                    <h1 className="content__title">Магнітний планер</h1>
-                    <div className="content__banner">
-                        <img className="content__bannerImage" src={ image1 } alt=""/>
-                    </div>
+    handleBackgroundChange = (event) => {
+        this.setState({
+            picture: event.currentTarget.value
+        }, () => {
+            console.log(this.picture);
+        })
+    }
 
-                    <p className="content__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, excepturi dolores. Incidunt commodi voluptates architecto culpa similique corrupti officia voluptatibus eligendi totam eveniet! Quidem unde incidunt ex eligendi, aperiam eaque.</p>
+    render() {
+        const { step, allSteps, type, price, picture } = this.state;
+        const { handleStepChange, handleTypeChange, handleBackgroundChange, handleImageUpload } = this;
+        return (
+            <React.Fragment>
+                <header className="header">
+                    {/* will insert logo and something more */}
+                </header>
 
-                    <div className="content__slider">
-                        <div className="slide">
-                            <img className="slide__image" src={ image2 } alt="" srcset="" />
+                {step === 0 ? (<div>
+                    <article className="content">
+                        <h1 className="content__title">Магнітний планер</h1>
+                        <div className="content__banner">
+                            <img className="content__bannerImage" src={ image1 } alt=""/>
                         </div>
-                        <div className="slide">
-                            <img className="slide__image" src={ image3 } alt="" srcset="" />
+
+                        <p className="content__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, excepturi dolores. Incidunt commodi voluptates architecto culpa similique corrupti officia voluptatibus eligendi totam eveniet! Quidem unde incidunt ex eligendi, aperiam eaque.</p>
+
+                        <div className="content__slider">
+                            <div className="slide">
+                                <img className="slide__image" src={ image2 } alt=""  />
+                            </div>
+                            <div className="slide">
+                                <img className="slide__image" src={ image3 } alt=""  />
+                            </div>
                         </div>
-                    </div>
 
-                    <p className="content__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, excepturi dolores. Incidunt commodi voluptates architecto culpa similique corrupti officia voluptatibus eligendi totam eveniet! Quidem unde incidunt ex eligendi, aperiam eaque.</p>
+                        <p className="content__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, excepturi dolores. Incidunt commodi voluptates architecto culpa similique corrupti officia voluptatibus eligendi totam eveniet! Quidem unde incidunt ex eligendi, aperiam eaque.</p>
 
-                </article>
+                    </article>
 
-                <footer className="footer">
-                    <div className="action action__step1" onClick={() => handleStepChange(1)}>
-                        <span className="action__title">Зконфігурувати {step}</span>
-                    </div>
-                </footer>
-            </div>) : ``}
-
-            {step === 1 ? (<div>
-                <article className="content">
-                    <h1 className="content__title">Тип сітки</h1>
-                    <section className="type">
-                        <input
-                            value='type 1'
-                            type="radio"
-                            name="type"
-                            id="type1"
-                            onChange={handleTypeChange}
-                        />
-                        <label className="type__wrapper" checked htmlFor="type1">
-                            <h3 className="type__title">Type 1</h3>
-                            <img className="type__image" src={image1} alt="Type 1" />
-                        </label>
-
-                        <input
-                            value='type 2'
-                            type="radio"
-                            name="type"
-                            id="type2"
-                            onChange={handleTypeChange}
-                        />
-                        <label className="type__wrapper" htmlFor="type2">
-                            <h3 className="type__title">Type 2</h3>
-                            <img className="type__image" src={image2} alt="Type 2" />
-                        </label>
-
-                        <input
-                            value='type 3'
-                            type="radio"
-                            name="type"
-                            id="type3"
-                            onChange={handleTypeChange}
-                        />
-                        <label className="type__wrapper" htmlFor="type3">
-                            <h3 className="type__title">Type 3</h3>
-                            <img className="type__image" src={image3} alt="Type 3" />
-                        </label>
-                    </section>
-                </article>
-
-                <footer className="footer">
-                    <div className="action action__step2" onClick={() => handleStepChange(2)}>
-                        <div className="action__info">
-                            <span className="action__step">Шаг {step} из {allSteps}</span>
+                    <footer className="footer">
+                        <div className="action action__step1" onClick={() => handleStepChange(1)}>
+                            <span className="action__title">Зконфігурувати {step}</span>
                         </div>
-                        <span className="action__title">Зконфігурувати {step}</span>
-                    </div>
-                </footer>
-            </div>) : ``}
+                    </footer>
+                </div>) : ``}
 
-            {step === 2 ? (<div>
-                <article className="content">
-                    <h1 className="content__title">Фон дошки</h1>
+                {step === 1 ? (<div>
+                    <article className="content">
+                        <h1 className="content__title">Тип сітки</h1>
+                        <section className="type">
+                            <input
+                                value={type1}
+                                type="radio"
+                                name="type"
+                                id="type1"
+                                onChange={handleTypeChange}
+                            />
+                            <label className="type__wrapper" checked htmlFor="type1">
+                                <h3 className="type__title">Type 1</h3>
+                                <img className="type__image" src={type1} alt="Type 1" />
+                            </label>
 
-                    <section className="type type--background">
-                        <input
-                            value='apple'
-                            type="radio"
-                            name="type"
-                            id="apple"
-                            onChange={handleBackgroundChange}
-                        />
-                        <label className="type__wrapper" checked htmlFor="apple">
-                            <img className="type__image" src={image1} alt="apple" />
-                        </label>
+                            <input
+                                value={type2}
+                                type="radio"
+                                name="type"
+                                id="type2"
+                                onChange={handleTypeChange}
+                            />
+                            <label className="type__wrapper" htmlFor="type2">
+                                <h3 className="type__title">Type 2</h3>
+                                <img className="type__image" src={type2} alt="Type 2" />
+                            </label>
 
-                        <input
-                            value='banana'
-                            type="radio"
-                            name="type"
-                            id="banana"
-                            onChange={handleBackgroundChange}
-                        />
-                        <label className="type__wrapper" htmlFor="banana">
-                            <img className="type__image" src={image2} alt="banana" />
-                        </label>
+                            <input
+                                value={image3}
+                                type="radio"
+                                name="type"
+                                id="type3"
+                                onChange={handleTypeChange}
+                            />
+                            <label className="type__wrapper" htmlFor="type3">
+                                <h3 className="type__title">Type 3</h3>
+                                <img className="type__image" src={image3} alt="Type 3" />
+                            </label>
+                        </section>
+                    </article>
 
-                        <input
-                            value='berry'
-                            type="radio"
-                            name="type"
-                            id="berry"
-                            onChange={handleBackgroundChange}
-                        />
-                        <label className="type__wrapper" htmlFor="berry">
-                            <img className="type__image" src={image3} alt="berry" />
-                        </label>
-                    </section>
-                </article>
-
-                <footer className="footer">
-                    <div className="action action__step2" onClick={() => handleStepChange(3)}>
-                        <div className="action__info">
-                            <span className="action__step">Шаг {step} из {allSteps}</span>
-                            <span className="action__price">{price} грн.</span>
+                    <footer className="footer">
+                        <div className="action action__step2" onClick={() => handleStepChange(2)}>
+                            <div className="action__info">
+                                <span className="action__step">Шаг {step} из {allSteps}</span>
+                            </div>
+                            <span className="action__title">Зконфігурувати {step}</span>
                         </div>
-                        <span className="action__title">Зконфігурувати {step}</span>
-                    </div>
-                </footer>
-            </div>) : ``}
+                    </footer>
+                </div>) : ``}
 
-        </React.Fragment>
+                {step === 2 ? (<div>
+                    <article className="content">
+                        <h1 className="content__title">Фон дошки</h1>
 
-    );
+                        <section className="type type--background">
+
+                            <ImageUploader
+                                withIcon={true}
+                                buttonText='Завантажте ваше зображення'
+                                onChange={handleImageUpload}
+                                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                                maxFileSize={5242880}
+                                singleImage={true}
+                                withPreview={true}
+                            />
+
+
+                            <input
+                                value={image1}
+                                type="radio"
+                                name="type"
+                                id="apple"
+                                onChange={handleBackgroundChange}
+                            />
+                            <label className="type__wrapper" checked htmlFor="apple">
+                                <img className="type__image" src={image1} alt="apple" />
+                                <img className="type__image type__image--grid" src={type} alt="apple" />
+                            </label>
+
+                            <input
+                                value={image2}
+                                type="radio"
+                                name="type"
+                                id="banana"
+                                onChange={handleBackgroundChange}
+                            />
+                            <label className="type__wrapper" htmlFor="banana">
+                                <img className="type__image" src={image2} alt="banana" />
+                                <img className="type__image type__image--grid" src={type} alt="apple" />
+                            </label>
+
+                            <input
+                                value={image3}
+                                type="radio"
+                                name="type"
+                                id="berry"
+                                onChange={handleBackgroundChange}
+                            />
+                            <label className="type__wrapper" htmlFor="berry">
+                                <img className="type__image" src={image3} alt="berry" />
+                                <img className="type__image type__image--grid" src={type} alt="apple" />
+                            </label>
+                        </section>
+                    </article>
+
+                    <footer className="footer">
+                        <div className="action action__step2" onClick={() => handleStepChange(3)}>
+                            <div className="action__info">
+                                <span className="action__step">Шаг {step} из {allSteps}</span>
+                                <span className="action__price">{price} грн.</span>
+                            </div>
+                            <span className="action__title">Зконфігурувати {step}</span>
+                        </div>
+                    </footer>
+                </div>) : ``}
+
+                {step === 3 ? (<div>
+                    <article className="content">
+                        <h1 className="content__title">Додаткове обладнання</h1>
+
+                        <section className="type type--background">
+                            <label className="type__wrapper" htmlFor="banana">
+                                <img className="type__image" src={picture} alt="banana" />
+                                <img className="type__image type__image--grid" src={type} alt="apple" />
+                            </label>
+                        </section>
+                    </article>
+
+                    <footer className="footer">
+                        <div className="action action__step2" onClick={() => handleStepChange(3)}>
+                            <div className="action__info">
+                                <span className="action__step">Шаг {step} из {allSteps}</span>
+                                <span className="action__price">{price} грн.</span>
+                            </div>
+                            <span className="action__title">Зконфігурувати {step}</span>
+                        </div>
+                    </footer>
+                </div>) : ``}
+
+            </React.Fragment>
+
+        );
+    }
+
 }
 
 export default Step1;
